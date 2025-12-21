da527a14d3a4c6f24eff1d1b3fe8453ef37a7a72
Pedro <lopes.pc.2019@gmail.com>
2025-12-21 19:19:31 -0300

    Adicionando exercicios

---
+++ CursoNelio/OperacoesComDateTime/OperacoesComDateTime/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Course
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime d = new DateTime(2001, 8, 15, 13, 45, 58);
+
+            DateTime d2 = d.AddHours(2);
+            DateTime d3 = d.AddMinutes(3);
+
+            DateTime d4 = d.AddDays(30);
+
+            Console.WriteLine(d);
+            Console.WriteLine(d2);
+            Console.WriteLine(d3);
+            Console.WriteLine(d4);
+
+            // Diferença entre datas:
+            DateTime dataAdmissao = new DateTime(2025, 07, 16);
+            DateTime dataAtual = new DateTime(2025, 12, 22);
+
+            TimeSpan t = dataAtual.Subtract(dataAdmissao);
+
+            Console.WriteLine(t);
+        }
+    }
+}
