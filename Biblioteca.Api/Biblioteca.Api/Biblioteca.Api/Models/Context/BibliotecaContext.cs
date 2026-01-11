8157a84ca682fb687ddb6d29147175d4a5afd09b
Pedro Lopes <lopes.pc.2019@gmail.com>
2026-01-11 01:26:48 -0300

    Issue #2 Criada entidade Editora adicionada a context EF. Closes #2

--- Biblioteca.Api/Biblioteca.Api/Biblioteca.Api/Models/Context/BibliotecaContext.cs
+++ Biblioteca.Api/Biblioteca.Api/Biblioteca.Api/Models/Context/BibliotecaContext.cs
@@ -11,12 +11,14 @@ namespace Biblioteca.Api.Models.Context
 
         public DbSet<Autor> Autores { get; set; }
         public DbSet<Livro> Livros { get; set; }
+        public DbSet<Editora> Editoras { get; set; }
 
         // Mapeando a tabela livros para o SQl
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.ApplyConfiguration(new AutorMapping());
             modelBuilder.ApplyConfiguration(new LivroMapping());
+            modelBuilder.ApplyConfiguration(new EditoraMapping());
         }
     }
 }
---
+++ Biblioteca.Api/Biblioteca.Api/Biblioteca.Api/Models/Entities/Editora.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Biblioteca.Api.Models.Entities
+{
+    [Table("Editoras")]
+    public class Editora
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string? Endereco { get; set; }
+        public string? Telefone { get; set; }
+        public string? Site { get; set; }
+        public string? EmailContato { get; set; }
+        public string? Observacao { get; set; }
+        public ICollection<Livro> Livros { get; set; }
+    }
+}
