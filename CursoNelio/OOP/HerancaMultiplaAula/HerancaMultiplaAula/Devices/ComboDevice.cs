fc8b9efc86561a3ff301ebb83cb6fd2bb0640574
Pedro Lopes <lopes.pc.2019@gmail.com>
2026-02-27 23:49:17 -0300

    Aula herança multipla, tentativa de implementação - Módulo Introdução POO

---
+++ CursoNelio/OOP/HerancaMultiplaAula/HerancaMultiplaAula/Devices/ComboDevice.cs
@@ -0,0 +1,9 @@
+﻿namespace HerancaMultiplaAula.Devices
+{
+
+    // Compilador naõ permite que seja feita herança multipla (problema do Diamante)
+    class ComboDevice : Scanner, Printer
+    {
+
+    }
+}
---
+++ CursoNelio/OOP/HerancaMultiplaAula/HerancaMultiplaAula/Devices/Printer.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HerancaMultiplaAula.Devices
+{
+    class Printer : Device
+    {
+        // Metodo abstrato é obrigado a sobrescrever
+        public override void ProcessDoc(string document)
+        {
+            Console.WriteLine("Printer processing: " + document);
+        }
+
+        public void Print(string document)
+        {
+            Console.WriteLine("Printer print " + document);
+        }
+    }
+}
