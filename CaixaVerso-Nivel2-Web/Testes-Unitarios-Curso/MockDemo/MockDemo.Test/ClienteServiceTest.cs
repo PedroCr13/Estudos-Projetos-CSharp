d323195da4ca944440bf1c6a0ca11491b4f534e7
Pedro Lopes <lopes.pc.2019@gmail.com>
2026-03-07 22:47:36 -0300

    Teste unitario com mock usando extensao MOQ

---
+++ CaixaVerso-Nivel2-Web/Testes-Unitarios-Curso/MockDemo/MockDemo.Test/ClienteServiceTest.cs
@@ -0,0 +1,104 @@
+﻿using FluentAssertions;
+using MockDemo.Interfaces;
+using MockDemo.Models;
+using MockDemo.Services;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+    Instalar a dependência MOQ (pacote NuGet) 
+    Referenciar projeto de testes para o projeto principal.
+    MOQ é uma extensão da Microsoft
+    Da classe ClienteService serão testados os metodos ObterTodos() e o Adicionar()
+    ambos dependem da classe ClienteRepository (a qual será feito mock)
+    Também foi usada extensão FluentAssertions
+*/
+
+namespace MockDemo.Test
+{
+    public class ClienteServiceTest
+    {
+        // testar se ao adicionar um cliente ele válido
+        // blocos do teste: convensão triplo A: Arrange, Act e Assert
+        [Fact]
+        public void Adicionar_ClienteValido_ClienteAdicionado()
+        {
+            // Arange (organização do teste, variáveis, inicializações)
+
+            // Mock simulando o clienteRepository:
+            Mock<IClienteRepository> clienteRepository = new();
+
+            var clienteService = new ClienteService(clienteRepository.Object); // adicionar .Object
+            var cliente = new Cliente("Pedro", "Lopes", new DateTime(1990, 05, 01), "pedro@lopes.com");
+
+            // Act (o que será feito)
+            clienteService.Adicionar(cliente);
+
+            // Assert (resultado esperado)
+            Assert.True(cliente.EhValido());
+
+            // verificar se um metodo da dependencia clienteRepository foi chamado ao menos uma vez:
+            // usando Verify do MOQ
+            clienteRepository.Verify(x => x.Adicionar(cliente), Times.Once);
+
+            // Verificar se o Adicionar do objeto mocado nunca foi chamado:
+            // clienteRepository.Verify(x => x.Adicionar(cliente), Times.Never);
+        }
+
+        [Fact]
+        public void Adicionar_ClienteValido_ClienteInvalido()
+        {
+            // Mock simulando o clienteRepository:
+            Mock<IClienteRepository> clienteRepository = new();
+
+            var clienteService = new ClienteService(clienteRepository.Object);
+
+            // cliente sem nome e sem email para falhar:
+            var cliente = new Cliente("", "Lopes", new DateTime(1990, 05, 01), "");
+
+            clienteService.Adicionar(cliente);
+
+            Assert.True(cliente.EhValido());
+
+            // verificar se um metodo da dependencia clienteRepository foi chamado ao menos uma vez:
+            // usando Verify do MOQ
+            clienteRepository.Verify(x => x.Adicionar(cliente), Times.Once);
+        }
+
+        // MOQ permite simulacao de retornos da classe simulada:
+        [Fact]
+        public void ObterTodos_RetorarTodosOsClientes()
+        {
+            // Arange:
+            var cliente1 = new Cliente("Pedro", "Lopes", new DateTime(1990, 05, 01), "pedro@lopes.com");
+            var cliente2 = new Cliente("Joao", "Silva", new DateTime(1980, 05, 01), "joao@silva.com");
+
+            var listaBanco = new List<Cliente>
+            {
+                cliente1,
+                cliente2
+            };
+
+            Mock<IClienteRepository> clienteRepository = new();
+            var clienteService = new ClienteService(clienteRepository.Object);
+
+            // configurar objeto mokado para quanto chamado metodo ObterTodos
+            // retornar a lista de clientes (ambiente comportamento controlado)
+            clienteRepository.Setup(x => x.ObterTodos())
+                .Returns(listaBanco);
+
+            // Act
+            var result = clienteService.ObterTodos();
+
+            // Assert (usando FluentAssertions para verificar se retornou 2 clientes)
+            result.Should().HaveCount(2);
+
+            // verificar se ObterTodos() foi chamado ao menos uma vez
+            clienteRepository.Verify(x => x.ObterTodos(), Times.Once);
+        }
+    }
+}
--- CaixaVerso-Nivel2-Web/Testes-Unitarios-Curso/MockDemo/MockDemo/Services/ClienteService.cs
+++ CaixaVerso-Nivel2-Web/Testes-Unitarios-Curso/MockDemo/MockDemo/Services/ClienteService.cs
@@ -8,13 +8,19 @@ using System.Threading.Tasks;
 
 /*
     Usando MOQ serão criados testes para a classe ClienteService. 
- 
+
+    Mock: teste de uma classe especifica sem se precupar com as dependências
+    será criada uma classe simulado a dependência que imitará seu comportamento
+    imita o comportamento do objeto de forma controlada e pré definida (como um ator)
+    será criado um projeto de testes e nele instalada a extensão MOQ.
 */
 
 namespace MockDemo.Services
 {
     public class ClienteService : IClienteService
     {
+        // Depedência injetada usada em todos os metódos
+        // para o teste será necessário usar mock
         private readonly IClienteRepository _clienteRepository;
 
         public ClienteService(IClienteRepository clientRepository                )
