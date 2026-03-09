f3124d45eda083c991e4206547ea7632fbacd57f
Pedro Lopes <lopes.pc.2019@gmail.com>
2026-03-09 13:21:21 -0300

    Teste usando o padrão TestDataBuilder para criar objetos

---
+++ CaixaVerso-Nivel2-Web/Testes-Unitarios-Curso/DataBuilderTests/DataBuilderTests/UserBuilder.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataBuilderTests
+{
+    public class UserBuilder
+    {
+        private User _user = new User();
+
+        public UserBuilder WithUserName(string username)
+        { 
+            _user.UserName = username;
+            return this;
+        }
+
+        public UserBuilder WithEmail(string email)
+        { 
+            _user.Email = email;
+            return this;
+        }
+
+        public UserBuilder WithDataOfBirth(DateTime dateOfBirth)
+        { 
+            _user.DateOfBirth = dateOfBirth;
+            return this;
+        }
+
+        public User Build()
+        { 
+            return _user;
+        }
+    }
+}
---
+++ CaixaVerso-Nivel2-Web/Testes-Unitarios-Curso/DataBuilderTests/DataBuilderTests/UserTest.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+/*
+    TestDataBuilder: Padrão de design que permite a construção de objetos
+    complexos para testar de forma fluente e flexível
+     
+    Cria objetos com valores padrão 
+    Não é uma extensão, é uma forma de escrever os construtores para gerar dados
+*/
+
+namespace DataBuilderTests
+{
+    public class UserTest
+    {
+        [Fact]
+        public void CreateUser_ValidData_Sucess()
+        {
+            // Construção do usuário utilizado o builder:
+            // cada metódo está retornando um UserBbuild
+            var user = new UserBuilder()
+                .WithUserName("José")
+                .WithEmail("jose@silva.com")
+                .WithDataOfBirth(new DateTime(1990, 5, 14))
+                .Build();
+
+            // Act
+            // ...
+            // Asserts
+            // ...
+        }
+    }
+}
