24bf6346f56671acedc1e4b585546455dfe6346c
Pedro Lopes <lopes.pc.2019@gmail.com>
2026-03-09 13:58:51 -0300

    Aula teste com Fixture (compartilhando uma instância de objeto com mais de uma caso de teste)

---
+++ CaixaVerso-Nivel2-Web/Testes-Unitarios-Curso/FixturesTests/InMemoryDbContextFixture.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+    Fixtures: variável comum a todos os testes (contextos compartilhados)
+    útil em situações onde há a necessidade de criar uma única instância de uma variável
+    que será acessada por todos os casos de testes de uma classe
+      
+    IClassFixture<T> (Xunit)
+*/
+
+namespace FixturesTests
+{
+    public class InMemoryDbContextFixture
+    {
+        public InMemoryDbContext Context { get; private set; }
+
+        // instanciar a classe de contexto
+        public InMemoryDbContextFixture()
+        {
+            Context = new InMemoryDbContext();
+
+            // inicializar dados iniciais
+        }
+
+        public void Dispose()
+        { 
+            // limpeza do banco dados...
+        }
+    }
+}
---
+++ CaixaVerso-Nivel2-Web/Testes-Unitarios-Curso/FixturesTests/InMemoryDbContextTest.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FixturesTests
+{
+    public class InMemoryDbContext : IClassFixture<InMemoryDbContext>
+    {
+        // objeto não será criado a cada execução de um teste, será compartilhado com todos
+        InMemoryDbContextFixture _fixture;
+
+        public InMemoryDbContext(InMemoryDbContextFixture fixture)
+        {
+            _fixture = fixture;
+        }
+
+
+        [Fact]
+        public void WithNoItens_CoutShouldReturnZero()
+        {
+            var count = _fixture.Context.Users.Count();
+            Assert.Equal(0, count);
+        }
+
+        [Fact]
+        public void AfterAddingItem_CountShouldReturnOne()
+        {
+            _fixture.Context.Users.Add(new User());
+            var count = _fixture.Context.Users.Count();
+            Assert.Equal(1, count);
+        }
+    }
+}
