37125f6b748f18e2a2fbc1b4cf658c09a8a4c030
Pedro Lopes <lopes.pc.2019@gmail.com>
2026-03-10 16:11:35 -0300

    Testando chamando API externa exemplo

---
+++ CaixaVerso-Nivel2-Web/Testes-Unitarios-Curso/ApiDemo/ApiDemo/Controllers/ValuesController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ApiDemo.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(new string[] { "value1", "value2" });
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult Get(int id)
+        {
+            return Ok($"value{id}");
+        }
+
+        [HttpPost]
+        public IActionResult Post([FromBody] string value)
+        {
+            return CreatedAtAction(nameof(Get), new { id = 3 }, value);
+        }
+    }
+}
---
+++ CaixaVerso-Nivel2-Web/Testes-Unitarios-Curso/ApiDemo/ApiDemoTests/ValuesControllerTest.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.VisualStudio.TestPlatform.TestHost;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit.Sdk;
+
+/*
+ *  Testando retorno de API
+    Instalar extensão: Microsoft.AspNetCore.Mvc.Testing 
+*/
+
+namespace ApiDemoTests
+{
+    //  IClassFixture: os serviços que estiverem definios serão compartilhado entre todos os testes
+    //  WebApplicationFactory irá rodar a API sem necessidade de executar o projeto principal.
+    public class ValuesControllerTest : IClassFixture<WebApplicationFactory<Program>>
+    {
+        // HttpClient: usando para fazer chamadas as APIs (legado)
+        private readonly HttpClient _client;
+
+        // na Program estão as configurações da API (entrypoint)
+        // com base na program a WebApplicationFactory cria um cliente para consumir a API
+        // o webapplication será compartilhado.
+        public ValuesControllerTest(WebApplicationFactory<Program> factory)
+        { 
+           _client = factory.CreateClient();
+        }
+
+        [Fact]
+        public async Task Get_ReturnsValues()
+        {
+            // Act
+            var response = await _client.GetAsync("/api/values");
+
+            // Assert
+            response.EnsureSuccessStatusCode(); // verifica se teve retorno de sucesso
+            var responseString = await response.Content.ReadAsStringAsync();
+
+            Assert.Contains("value1", responseString);
+            Assert.Contains("value2", responseString);
+        }
+
+        [Fact]
+        public async Task Get_WithIdReturnsValue()
+        {
+            // act
+            var response = await _client.GetAsync("/api/values/1");
+
+            // Assert
+            response.EnsureSuccessStatusCode();
+            var responseString = await response.Content.ReadAsStringAsync();
+            Assert.Equal("value1", responseString);
+        }
+
+        [Fact]
+        public async Task Post_CreatesValue()
+        {
+            // Arrange:
+            var content = new StringContent("\"value3\"", System.Text.Encoding.UTF8, "Application/json");
+
+            // act
+            var response = await _client.PostAsync("/api/values", content);
+
+            // Assert
+            response.EnsureSuccessStatusCode();
+            Assert.Equal(System.Net.HttpStatusCode.Created, response.StatusCode);
+            var responseString = await response.Content.ReadAsStringAsync();
+            Assert.Equal("value3", responseString);
+        }
+    }
+}
