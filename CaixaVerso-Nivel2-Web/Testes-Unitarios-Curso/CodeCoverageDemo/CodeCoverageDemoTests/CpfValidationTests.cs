952b5543d01d0907abf4db05e5b35b5ae9145d60
Pedro Lopes <lopes.pc.2019@gmail.com>
2026-03-13 01:46:09 -0300

    Aula testes com Fine Code Coverage (verificado cobertura dos testes)

--- CaixaVerso-Nivel2-Web/Testes-Unitarios-Curso/CodeCoverageDemo/CodeCoverageDemoTests/CpfValidationTests.cs
+++ CaixaVerso-Nivel2-Web/Testes-Unitarios-Curso/CodeCoverageDemo/CodeCoverageDemoTests/CpfValidationTests.cs
@@ -6,6 +6,20 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+/**
+    Boas práticas:
+
+    Desenvolvimento guiado por testes (TDD)
+    Refatoração continua
+    Análise de risco e priorização (partes com maior impacto, criticos)
+    Teste de regressão (novas funcionalidades não quebrem já existentes)
+    Mocking (isolar partes do sistema) e Stubbing 
+    Revisão de código e testes (cobertura e qualidade)
+    Testes em diversos ambientes (navegadores, SOs)
+    Treinamento e cultura de teste
+    Métricas de cobertura
+*/
+
 namespace CodeCoverageDemoTests
 {
     public class CpfValidationTests
