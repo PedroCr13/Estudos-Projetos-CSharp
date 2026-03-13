d6be4abf663b1e4dcc5e5e592e0879b14d0295cd
Pedro Lopes <lopes.pc.2019@gmail.com>
2026-03-13 01:37:12 -0300

    Aula testes com Fine Code Coverage (verificado cobertura dos testes)

---
+++ CaixaVerso-Nivel2-Web/Testes-Unitarios-Curso/CodeCoverageDemo/CodeCoverageDemo/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace CodeCoverageDemo
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        { 
+            CpfValidation cpfValidation = new CpfValidation();
+
+            string cpf = "051.035.860-88";
+            string resultadoValdacao = cpfValidation.EhValido(cpf) ? "CPF Válido" : "CPF Inválido";
+
+            System.Console.WriteLine(resultadoValdacao);
+        }
+    }
+}
---
+++ CaixaVerso-Nivel2-Web/Testes-Unitarios-Curso/CodeCoverageDemo/CodeCoverageDemoTests/CpfValidationTests.cs
@@ -0,0 +1,42 @@
+﻿using CodeCoverageDemo;
+using FluentAssertions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeCoverageDemoTests
+{
+    public class CpfValidationTests
+    {
+        // cada linha é um teste 
+        [Theory]
+        [InlineData("15231766607")]
+        [InlineData("78246847333")]
+        [InlineData("64184957307")]
+        [InlineData("21681764423")]
+        [InlineData("13830803800")]
+        public void Cpf_ValidarMultiplisNumeros_TodosDevemSerValidados(string cpf)
+        {
+            // Arrange
+            var cpfValidation = new CpfValidation();
+
+            // Act and Assert
+            cpfValidation.EhValido(cpf).Should().BeTrue();
+        }
+
+        [Theory]
+        [InlineData("15231766")]
+        [InlineData("1523176611111111")]
+        [InlineData("1111")]
+        public void Cpf_ValidarMultiplisNumeros_TodosDevemSerInvalidos(string cpf)
+        {
+            // Arrange
+            var cpfValidation = new CpfValidation();
+
+            // Act and Assert
+            cpfValidation.EhValido(cpf).Should().BeFalse();
+        }
+    }
+}
