9130605a1319c16b50e02c543a7a6d797d7d8ab4
Pedro Lopes <lopes.pc.2019@gmail.com>
2026-03-18 09:42:27 -0300

    Documentando API com Swagger

---
+++ CaixaVerso-Nivel2-Web/AulaDocAPI/src/Aula.Api/Controllers/WeatherForecastController.cs
@@ -0,0 +1,40 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace Aula.Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        /// <summary>
+        /// EndPoint para retornar a previsão do tempo de hoje e dos próximos 5 dias
+        /// </summary>
+        /// <response code="200">Lista de previsões para os próximos dias</response>
+        /// <response code="404">Estamos sem previsão do tempo</response>
+        // Obs: adicionar propety group no csproj: <GenerateDocumentationFile>true</GenerateDocumentationFile>
+        // e também na program em SwaggerGer adicionar inclusão do XML
+        [HttpGet(Name = "GetWeatherForecast")]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+    }
+}
---
+++ CaixaVerso-Nivel2-Web/AulaDocAPI/src/Aula.Api/Program.cs
@@ -0,0 +1,59 @@
+/*
+    Swagger permite documentar a API
+    - customizar informações
+    - Customizar comentários XML
+    - Documentar autenticação e autorização
+    - Documentar versionamento de API
+
+    baseada no padrão OpenAPI
+ */
+
+using System.Reflection;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
+// Documentação
+builder.Services.AddEndpointsApiExplorer();  
+
+// adicionar informações adcionais sobre API:
+builder.Services.AddSwaggerGen(c => 
+{
+    c.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo
+    {
+        Title = "Aula.Api",
+        Version = "v1",
+        Description = "Aula demonstrando documentação de API",
+        Contact = new Microsoft.OpenApi.Models.OpenApiContact
+        {
+            Name = "Pedro Lopes",
+            Email = "pedro@email.com.br"
+        }
+    });
+
+    var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+    var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+    c.IncludeXmlComments(xmlPath);
+}); 
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    // expor endpoints documentação
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
