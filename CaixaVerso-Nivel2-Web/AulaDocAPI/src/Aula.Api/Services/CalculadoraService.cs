93aa2dc692468dcbef6b4c950e3cf9c0e7cafe37
Pedro Lopes <lopes.pc.2019@gmail.com>
2026-03-19 09:36:53 -0300

    Teste unitário com XUnit

---
+++ CaixaVerso-Nivel2-Web/AulaDocAPI/src/Aula.Api/Services/CalculadoraService.cs
@@ -0,0 +1,25 @@
+﻿namespace Aula.Api.Services
+{
+    public class CalculadoraService
+    {
+        public int Somar(int a, int b)
+        { 
+            return a + b;
+        }
+
+        public int Subtrair(int a, int b)
+        {
+            return a - b;
+        }
+
+        public int Multiplicar(int a, int b)
+        {
+            return a * b;
+        }
+
+        public int Dividir(int a, int b)
+        {
+            return a / b;
+        }
+    }
+}
