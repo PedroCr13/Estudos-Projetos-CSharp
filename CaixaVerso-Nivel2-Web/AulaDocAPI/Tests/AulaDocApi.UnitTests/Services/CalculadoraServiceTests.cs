57858fc995613120e44f0d595db1786f16e1638d
Pedro Lopes <lopes.pc.2019@gmail.com>
2026-03-19 09:38:42 -0300

    Teste com XUnit

---
+++ CaixaVerso-Nivel2-Web/AulaDocAPI/Tests/AulaDocApi.UnitTests/Services/CalculadoraServiceTests.cs
@@ -0,0 +1,45 @@
+﻿using Aula.Api.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+    Testes unitários são os testes mais baratos
+    São investimentos de longo prazo, 
+    úteis quando moficações no projeto para verificar se houve erros de funcionalidades
+*/
+
+namespace AulaDocApi.UnitTests.Services
+{
+    public class CalculadoraServiceTests
+    {
+        [Fact]
+        public void Somar_DeveRetornarSomaCorreta()
+        {
+            // Arrange
+            var numero1= 10;
+            var numero2= 20;
+
+            // Act
+            var calculadoraService = new CalculadoraService();
+            var soma = calculadoraService.Somar(numero1, numero2);
+
+            // Assert (esperado, resultado)
+            Assert.Equal(30, soma);    
+        }
+
+        [Theory]
+        [InlineData(20, 10, 10)]
+        [InlineData(10, 3, 7)]
+        public void Subtrair_DeveRetornarSubtracaoCorretamente(int n1, int n2, int resultado)
+        {
+
+            var calculadoraService = new CalculadoraService();
+            var subtracao = calculadoraService.Subtrair(n1, n2);
+
+            Assert.Equal(resultado, subtracao);
+        }
+    }
+}
